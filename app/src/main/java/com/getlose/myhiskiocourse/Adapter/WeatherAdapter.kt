01cb2116475a6901db81421c57943ae205b9720d
jenhsiang <getlose2011@gmail.com>
2022-06-28 23:03:21 +0800

    10-2 增加多個ViewHolder

--- app/src/main/java/com/getlose/myhiskiocourse/Adapter/WeatherAdapter.kt
+++ app/src/main/java/com/getlose/myhiskiocourse/Adapter/WeatherAdapter.kt
@@ -1,29 +1,31 @@
 package com.getlose.myhiskiocourse.Adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.getlose.myhiskiocourse.Models.Location
 import com.getlose.myhiskiocourse.databinding.RowItemWeatherLayoutBinding
+import com.getlose.myhiskiocourse.databinding.RowItemWeatherOddLayoutBinding
+import java.lang.Exception
 
-class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
+//用到二個 XML VIEW
+class WeatherAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    val TAG = "WeatherAdapter"
     var items: List<Location>? = null
+    private var VIEW_TYPE_ODD = 0
+    private var VIEW_TYPE_EVEN = 1
 
     fun updateItems(items: List<Location>?){
         this.items = items
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
-        val binding = RowItemWeatherLayoutBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return WeatherViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
-        if (holder is WeatherViewHolder) {
-            holder.bindData(items!![position])
+    override fun getItemViewType(position: Int): Int {
+        if (position%2==0){
+            return VIEW_TYPE_ODD
+        }else{
+            return VIEW_TYPE_EVEN
         }
     }
 
@@ -45,6 +47,57 @@ class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() 
             viewBinding.minT.text = "$minT °C"
             viewBinding.maxT.text = "$maxT °C"
             viewBinding.wx.text = wx
+        }
+    }
+
+    inner class WeatherViewHolder2(val viewBinding: RowItemWeatherOddLayoutBinding) :RecyclerView.ViewHolder(viewBinding.root){
+
+        fun bindData(item: Location){
+            val wx = item.weatherElement?.find { it.elementName == "Wx" }?.time?.first()?.parameter?.parameterName
+            val pop = item.weatherElement?.find { it.elementName == "PoP" }?.time?.first()?.parameter?.parameterName
+            val minT = item.weatherElement?.find { it.elementName == "MinT" }?.time?.first()?.parameter?.parameterName
+            val maxT = item.weatherElement?.find { it.elementName == "MaxT" }?.time?.first()?.parameter?.parameterName
+
+            viewBinding.location.text = item.locationName
+            viewBinding.pop.text = "降雨機率:$pop%"
+            viewBinding.minT.text = "$minT °C"
+            viewBinding.maxT.text = "$maxT °C"
+            viewBinding.wx.text = wx
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        Log.d(TAG, "onCreateViewHolder: $viewType")
+        if(viewType == VIEW_TYPE_EVEN){
+            val binding = RowItemWeatherLayoutBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
+
+            return WeatherViewHolder(binding)
+        }else if ( viewType == VIEW_TYPE_ODD){
+            val binding = RowItemWeatherOddLayoutBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
+
+            return WeatherViewHolder2(binding)
+        }
+        else{
+            throw Exception("Unknown viewType:${viewType}")
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val data = items?.get(position)
+        if (holder is WeatherViewHolder){
+            if (data != null) {
+                holder.bindData(data)
+            }
+        }else if (holder is WeatherViewHolder2){
+
+            if (data != null) {
+                holder.bindData(data)
+            }
+        }else{
+            throw Exception("Unknown holder:${holder.javaClass}")
         }
     }
 
